7894666a01e55e900b665047ebbc197a9935f362
Natália Caroline <nataliacarolinesenai@gmail.com>
2024-08-05 15:18:15 -0300

    piscarled/

--- piscarled/piscarled.ino
+++ piscarled/piscarled.ino
@@ -1,9 +1,20 @@
+//Programa que envia sinal alto para a saída 7,
+//aguarda 1 segundo e envia sinal baixo para a saída 7
 void setup() {
   // put your setup code here, to run once:
+  //  porta configurada como saída
 
+  pinMode(7,OUTPUT);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //  envia sinal alto
+  digitalWrite(7,HIGH);
+  delay(1000);
+  //  envia sinal baixo
+  digitalWrite(7,LOW);
+  delay(1000);
 
 }
