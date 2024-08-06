f6cd8b93f52bddc0d4da74478c10a22df512f901
Natália <nataliacarolinesenai@gmail.com>
2024-08-06 09:02:52 -0300

    piscar2leds
    código para piscar 2 leds

---
+++ piscar2leds/piscar2leds.ino
@@ -0,0 +1,17 @@
+void setup(){
+  //  Configure os pinos como saída
+  pinMode(7,OUTPUT);
+  pinMode(8,OUTPUT);
+}
+
+void loop(){
+  //  Pisca o LED no pino 7
+  digitalWrite(7, HIGH);
+  digitalWrite(8,HIGH);
+  delay(1000); //espera por 1 segundo
+
+  //  Pisca o LED no pino 8
+  digitalWrite(7,LOW);
+  digitalWrite(8,HIGH);
+  delay(1000); // espera por 1 segundo
+}
